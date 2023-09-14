e0212604a330758038b2a5269694ba70ea0d5dc6
arthur <aazl121287@gmail.com>
2023-09-13 23:07:29 -0300

    construtor-metodos de clases

---
+++ vehiculo.py
@@ -0,0 +1,18 @@
+class Auto:
+    # constructor
+    def __init__(self, marca, modelo, rendimiento,motor):
+        # atributos
+        self.marca = marca
+        self.modelo = modelo
+        self.motor = motor
+        self.rendimiento = rendimiento
+    
+    # métodos   
+    def gasto(self, kms):
+        return kms / self.rendimiento
+    
+tida = Auto('Nissan', 'Tida', 13, 1600)
+subaru = Auto('Subaru', 'Forester', 11, 2000)
+chevy = Auto('Chevrolet', 'Prisma', 12, 1400)
+
+import pdb; pdb.set_trace()
